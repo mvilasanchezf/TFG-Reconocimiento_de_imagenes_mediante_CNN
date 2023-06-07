eb37b7cd623161d8a2d51be065b228b48ea81ac2
Martin VF <mvilasanchezf@alumnos.nebrija.es>
2023-06-07 19:34:32 +0200

    Demo App v1.2

--- Aplicacion/app.py
+++ Aplicacion/app.py
@@ -6,12 +6,13 @@ from PIL import Image
 
 app = Flask(__name__)
 
+#funcion para renderizar la pagina web
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
     
 
-
+#funcion para predecir la imagen subida por el usuario y devolver la prediccion en la pagina web
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
@@ -27,6 +28,9 @@ def predict():
             predict_prob, prediction_idx = get_prediction(input_tensor)
             class_name = render_prediction(prediction_idx)
 
+            #pasamos el predict prob a 2 digitos decimales
+            predict_prob = round(predict_prob, 2)
+
             #parseamos el json class_name para devolver las categorias de la imagen almacenadas en el json pasandolos a strings
             details = json.loads(class_name)
             common_name = details["common_name"]
@@ -38,7 +42,10 @@ def predict():
             genus = details["genus"]
             specific_epithet = details["specific_epithet"]
 
+            #creamos un diccionario con los datos de la prediccion
             response = {'prediction': {'accuracy': predict_prob, 'common_name': common_name, 'kingdom': kingdom, 'phylum': phylum, 'class': class_, 'order': order, 'family': family, 'genus': genus, 'specific_epithet': specific_epithet}}
+
+            #devolvemos la prediccion en a la pagina web
             return render_template('index.html', 
                                     user_image = image,
                                     prediction_text = "Con una precisión del {0} '%' ".format(response["prediction"]["accuracy"]), 
@@ -52,6 +59,6 @@ def predict():
                                     prediction_text9 =  " La especie es: {0}".format(response["prediction"]["specific_epithet"])
                                     )
                                      
-
+    return render_template('index.html', prediction_text="No se ha podido realizar la predicción, por favor, inténtelo de nuevo")
 if __name__ == '__main__':
     app.run(debug=True)
